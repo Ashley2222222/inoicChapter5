f8445e3d66fa7a761dd6436b62a0f3fe2617bb7a
YolandaLeung <tot.123@163.com>
2017-08-25 16:54:41 +0800

    finish

--- www/js/app.js
+++ www/js/app.js
@@ -18,10 +18,11 @@ angular.module('App', ['ionic'])
 		}
 	})
 	.state('tabs.history',{
-		url:'/history',
+		url:'/history?currency',
 		views:{
 			'history-tab':{
-				templateUrl:'views/history/history.html'
+				templateUrl:'views/history/history.html',
+				controller:'HistoryController'
 			}
 		}
 	})
@@ -29,7 +30,17 @@ angular.module('App', ['ionic'])
 		url:'/currencies',
 		views:{
 			'currencies-tab':{
-				templateUrl:'views/currencies/currencies.html'
+				templateUrl:'views/currencies/currencies.html',
+				controller:'CurrenciesController'
+			}
+		}
+	})
+	.state('tabs.detail', {
+		url: '/detail',
+		views: {
+			'rates-tab': {
+				templateUrl: 'views/detail/detail.html',
+				controller: 'DetailController'
 			}
 		}
 	});
@@ -46,27 +57,26 @@ angular.module('App', ['ionic'])
 		}
 	});
 })
-
 .factory('Currencies', function () {
-  return [
-    { code: 'AUD', text: 'Australian Dollar', selected: true },
-    { code: 'BRL', text: 'Brazilian Real', selected: false },
-    { code: 'CAD', text: 'Canadian Dollar', selected: true },
-    { code: 'CHF', text: 'Swiss Franc', selected: false },
-    { code: 'CNY', text: 'Chinese Yuan', selected: true},
-    { code: 'EUR', text: 'Euro', selected: true },
-    { code: 'GBP', text: 'British Pound Sterling', selected: true },
-    { code: 'IDR', text: 'Indonesian Rupiah', selected: false },
-    { code: 'ILS', text: 'Israeli New Sheqel', selected: false },
-    { code: 'MXN', text: 'Mexican Peso', selected: true },
-    { code: 'NOK', text: 'Norwegian Krone', selected: false },
-    { code: 'NZD', text: 'New Zealand Dollar', selected: false },
-    { code: 'PLN', text: 'Polish Zloty', selected: false },
-    { code: 'RON', text: 'Romanian Leu', selected: false },
-    { code: 'RUB', text: 'Russian Ruble', selected: true },
-    { code: 'SEK', text: 'Swedish Krona', selected: false },
-    { code: 'SGD', text: 'Singapore Dollar', selected: false },
-    { code: 'USD', text: 'United States Dollar', selected: true },
-    { code: 'ZAR', text: 'South African Rand', selected: false }
-  ];
+	return [
+	{ code: 'AUD', text: 'Australian Dollar', selected: true },
+	{ code: 'BRL', text: 'Brazilian Real', selected: false },
+	{ code: 'CAD', text: 'Canadian Dollar', selected: true },
+	{ code: 'CHF', text: 'Swiss Franc', selected: false },
+	{ code: 'CNY', text: 'Chinese Yuan', selected: true},
+	{ code: 'EUR', text: 'Euro', selected: true },
+	{ code: 'GBP', text: 'British Pound Sterling', selected: true },
+	{ code: 'IDR', text: 'Indonesian Rupiah', selected: false },
+	{ code: 'ILS', text: 'Israeli New Sheqel', selected: false },
+	{ code: 'MXN', text: 'Mexican Peso', selected: true },
+	{ code: 'NOK', text: 'Norwegian Krone', selected: false },
+	{ code: 'NZD', text: 'New Zealand Dollar', selected: false },
+	{ code: 'PLN', text: 'Polish Zloty', selected: false },
+	{ code: 'RON', text: 'Romanian Leu', selected: false },
+	{ code: 'RUB', text: 'Russian Ruble', selected: true },
+	{ code: 'SEK', text: 'Swedish Krona', selected: false },
+	{ code: 'SGD', text: 'Singapore Dollar', selected: false },
+	{ code: 'USD', text: 'United States Dollar', selected: true },
+	{ code: 'ZAR', text: 'South African Rand', selected: false }
+	];
 });
--- www/views/detail/detail.js
+++ www/views/detail/detail.js
@@ -1,12 +1,12 @@
 angular.module('App')
-.controller('DetailController',function($scope,$stateParam,$state,
-	Currencies){
-	angular.forEach(Currencies,function(currency{
-		if(currency.code==$stateParam.currency){
-			$scope.currency = currency;
-		}
-	});
-	if (angular.isUndefined($scope.currency.ticker)) {
-		$state.go('tabs.rates');
-	}
+.controller('DetailController', function ($scope, $stateParams, $state,
+Currencies) {
+// angular.forEach(Currencies, function (currency {
+// if (currency.code == $stateParams.currency) {
+// $scope.currency = currency;
+// }
+// });
+// if (angular.isUndefined($scope.currency.ticker)) {
+// $state.go('tabs.rates');
+// }
 });
